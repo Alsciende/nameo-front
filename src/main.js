42ed44b2ffe9aec129558ccb39d486a1afa06dd9
Cédric Bertolini <c.bertolini@wakeonweb.com>
2018-06-27 15:45:18 +0200

    install

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 
 import store from './store';
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
