89f83dd3c238d1ea5439cc0aea8dbc937ec99d30
Cédric Bertolini <c.bertolini@wakeonweb.com>
2018-06-27 15:08:41 +0200

    install

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
+import Vue from 'vue';
+import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
+
+import App from './App.vue';
 import store from './store';
+
+Vue.use(VueOnsen);
 
 Vue.config.productionTip = false;
 
